aafaa0b7e10796d9238ab9e0fc0f65963a32bd45
Kauê BBS <120749036+kauebbs@users.noreply.github.com>
2025-03-17 16:09:29 -0300

    Add files via upload

---
+++ EnxamePhobos.DAL/FilmeDAL.cs
@@ -0,0 +1,238 @@
+﻿using MySql.Data.MySqlClient;//
+using EnxamePhobos.DTO;//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnxamePhobos.DAL
+{
+    public class FilmeDAL:Conexao
+    {
+        //busca por nome
+        public FilmeDTO SearchFilme(string objSearch)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT filme.id,Titulo,Produtora,UrlImg,Genero_Id,Descricao FROM filme INNER JOIN Classificacao ON Classificacao_Id = Classificacao.Id WHERE Filme.Titulo = @Titulo;", conn);
+                cmd.Parameters.AddWithValue("@Nome", objSearch);
+                dr = cmd.ExecuteReader();
+                FilmeDTO obj = null;
+                if (dr.Read())
+                {
+                    obj = new FilmeDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Titulo = dr["Titulo"].ToString();
+                    obj.Produtora = dr["Produtora"].ToString();
+                    obj.UrlImg = dr["UrlImg"].ToString();
+                    obj.Genero_Id = dr["Genero_Id"].ToString();
+                    obj.Classificacao_Id = dr["Classificacao_Id"].ToString();
+                }
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Deu ruim search name !" + ex.Message);
+            }
+            finally { Desconectar(); }
+        }
+
+        //busca por ID
+        public FilmeDTO SearchId(int objSearch)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM filme WHERE filmes.Id = @Id;", conn);
+                cmd.Parameters.AddWithValue("@Id", objSearch);
+                dr = cmd.ExecuteReader();
+                FilmeDTO obj = null;
+                if (dr.Read())
+                {
+                    obj = new FilmeDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Titulo = dr["Titulo"].ToString();
+                    obj.Produtora = dr["Produtora"].ToString();
+                    obj.UrlImg = dr["UrlImg"].ToString();
+                    obj.Genero_Id = dr["Genero_Id"].ToString();
+                    obj.Classificacao_Id = dr["Classificacao_Id"].ToString();
+                }
+                return obj;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Deu ruim na busca por id !" + ex.Message);
+            }
+            finally { Desconectar(); }
+        }
+
+        //List
+        public List<FilmeDTO> Listar()
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT filme.id, titulo, produtora, urlimg, classificacao.descricao AS Classificacao, Genero.Descricao AS Genero FROM filme INNER JOIN Classificacao ON Classificacao_id = classificacao.id INNER JOIN Genero ON Genero_Id = Genero.id ORDER BY filme.id ASC;", conn);
+                dr = cmd.ExecuteReader();
+                List<FilmeDTO> Lista = new List<FilmeDTO>();//Lista Vazia
+                while (dr.Read())
+                {
+                    FilmeDTO obj = new FilmeDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Titulo = dr["Titulo"].ToString();
+                    obj.Produtora = dr["Produtora"].ToString();
+                    obj.UrlImg = dr["UrlImg"].ToString();
+                    obj.Genero_Id = dr["Genero"].ToString();
+                    obj.Classificacao_Id = dr["Classificacao"].ToString();
+                    Lista.Add(obj);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("LASCOU " + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        public void CadastrarFilme(FilmeDTO objCad)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("INSERT INTO filme (Titulo,Produtora,UrlImg,Classificacao_Id,Genero_Id) VALUES(@Titulo,@Produtora,@UrlImg,@Classificacao_Id,@Genero_Id)", conn);
+                cmd.Parameters.AddWithValue("@Titulo", objCad.Titulo);
+                cmd.Parameters.AddWithValue("Produtora", objCad.Produtora);
+                cmd.Parameters.AddWithValue("@UrlImg", objCad.UrlImg);
+                cmd.Parameters.AddWithValue("@Classificacao_Id", objCad.Classificacao_Id);
+                cmd.Parameters.AddWithValue("@Genero_Id", objCad.Genero_Id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Deu ruimkkk" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        public List<GeneroDTO> CarregaDDL()
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM Genero", conn);
+                dr = cmd.ExecuteReader();
+                List<GeneroDTO> Lista = new List<GeneroDTO>();
+                while (dr.Read())
+                {
+                    GeneroDTO obj = new GeneroDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Descricao = dr["Descricao"].ToString();
+                    Lista.Add(obj);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lascou" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //update
+        public void Update(FilmeDTO objUpdt)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("UPDATE Filme SET Titulo = @Titulo, Produtora = @Produtora, UrlImg = @UrlImg, Genero_Id = @Genero_Id, Classificacao_Id = @Classificacao_Id WHERE Filme.Id = @Id;", conn);
+                cmd.Parameters.AddWithValue("@Titulo", objUpdt.Titulo);
+                cmd.Parameters.AddWithValue("@Produtora", objUpdt.Produtora);
+                cmd.Parameters.AddWithValue("@UrlImg", objUpdt.UrlImg);
+                cmd.Parameters.AddWithValue("@Genero_Id", objUpdt.Genero_Id);
+                cmd.Parameters.AddWithValue("@Classificacao_Id", objUpdt.Classificacao_Id);
+                cmd.Parameters.AddWithValue("@Id", objUpdt.Id);
+                cmd.ExecuteNonQuery();
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+
+
+                throw new Exception("Deu ruim no update !" + ex.Message);
+            }
+            finally { Desconectar(); }
+        }
+
+
+        //delete
+        public void Delete(int objDel)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("DELETE FROM Filmee WHERE Filme.Id = @Id;", conn);
+                cmd.Parameters.AddWithValue("@Id", objDel);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Deu ruimkkkk no delete!" + ex.Message);
+            }
+            finally { Desconectar(); }
+        }
+
+        //Filtrar por genero
+        public List<FilmeDTO> FiltrarFilme(string objFilter)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT filme.Id, Titulo, Produtora, UrlImg, Classificacao.Descricao AS Classificacao, Genero.Descricao AS Genero FROM filme INNER JOIN Classificacao ON Classificacao_ID = classificacao.Id INNER JOIN Genero ON Genero_Id = Genero.id WHERE Genero.Descricao = @Genero;", conn);
+                cmd.Parameters.AddWithValue("genero", objFilter);
+                dr = cmd.ExecuteReader();
+                List<FilmeDTO> Lista = new List<FilmeDTO>();//Lista Vazia
+                while (dr.Read())
+                {
+                    FilmeDTO obj = new FilmeDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Titulo = dr["Titulo"].ToString();
+                    obj.Produtora = dr["Produtora"].ToString();
+                    obj.UrlImg = dr["UrlImg"].ToString();
+                    obj.Genero_Id = dr["Genero"].ToString();
+                    obj.Classificacao_Id = dr["Classificacao"].ToString();
+                    Lista.Add(obj);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("LASCOU " + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+    }
+}
---
+++ EnxamePhobos.DAL/UsuarioDAL.cs
@@ -0,0 +1,258 @@
+﻿using EnxamePhobos.DTO;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnxamePhobos.DAL
+{
+    public class UsuarioDAL : Conexao
+    {
+        //autenticar
+        public UsuarioDTO Autenticar(string nome, string senha)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT Nome, Senha, TipoUsuario_Id FROM Usuario WHERE Nome = @Nome And Senha = @Senha ", conn);
+                cmd.Parameters.AddWithValue("@Nome", nome);
+                cmd.Parameters.AddWithValue("@Senha", senha);
+                dr = cmd.ExecuteReader();
+                UsuarioDTO obj = null;
+                if (dr.Read())
+                {
+                    obj = new UsuarioDTO();
+                    obj.Nome = dr["Nome"].ToString();
+                    obj.Senha = dr["Senha"].ToString();
+                    obj.TipoUsuario_Id = dr["TipoUsuario_Id"].ToString();
+
+                }
+                return obj;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Usuário não cadastrado !" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+
+            }
+        }
+
+        //CRUD
+        //List
+        public List<UsuarioDTO> Listar()
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT Usuario.Id, Nome, Email, Senha, DataNascusuario, Descricao FROM usuario inner join tipousuario ON Usuario.TipoUsuario_Id = TipoUsuario.Id ORDER BY Usuario.Id ASC;", conn);
+                dr = cmd.ExecuteReader();
+                List<UsuarioDTO> Lista = new List<UsuarioDTO>();//Lista Vazia
+                while (dr.Read())
+                {
+                    UsuarioDTO obj = new UsuarioDTO();
+                    obj.ID = Convert.ToInt32(dr["Id"]);
+                    obj.Nome = dr["Nome"].ToString();
+                    obj.Email = dr["Email"].ToString();
+                    obj.Senha = dr["Senha"].ToString();
+                    obj.DataNascUsuario = Convert.ToDateTime(dr["DatanascUsuario"]);
+                    obj.TipoUsuario_Id = dr["Descricao"].ToString();
+                    Lista.Add(obj);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("LASCOU " + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+
+        //Carrega DropDownList
+        public List<TipoUsuarioDTO> CarregaDDL()
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM TipoUsuario", conn);
+                dr = cmd.ExecuteReader();
+                List<TipoUsuarioDTO> Lista = new List<TipoUsuarioDTO>();
+                while (dr.Read())
+                {
+                    TipoUsuarioDTO obj = new TipoUsuarioDTO();
+                    obj.Id = Convert.ToInt32(dr["Id"]);
+                    obj.Descricao = dr["Descricao"].ToString();
+                    Lista.Add(obj);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lascou" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //Busca por nome
+        public UsuarioDTO SearchName(string objSearch)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM usuario WHERE usuario.nome = @Nome;", conn);
+                cmd.Parameters.AddWithValue("@Nome", objSearch);
+                dr = cmd.ExecuteReader();
+                UsuarioDTO obj = null;
+                if (dr.Read())
+                {
+                    obj = new UsuarioDTO();
+                    obj.ID = Convert.ToInt32(dr["Id"]);
+                    obj.Nome = dr["Nome"].ToString();
+                    obj.Email = dr["Email"].ToString();
+                    obj.Senha = dr["Senha"].ToString();
+                    obj.DataNascUsuario = Convert.ToDateTime(dr["DataNascUsuario"]);
+                    obj.TipoUsuario_Id = dr["TipoUsuario_Id"].ToString();
+                }
+                return obj;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lascou" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //Busca por ID
+        public UsuarioDTO SearchID(int objSearch)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM usuario WHERE usuario.ID = @ID;", conn);
+                cmd.Parameters.AddWithValue("@ID", objSearch);
+                dr = cmd.ExecuteReader();
+                UsuarioDTO obj = null;
+                if (dr.Read())
+                {
+                    obj = new UsuarioDTO();
+                    obj.ID = Convert.ToInt32(dr["Id"]);
+                    obj.Nome = dr["Nome"].ToString();
+                    obj.Email = dr["Email"].ToString();
+                    obj.Senha = dr["Senha"].ToString();
+                    obj.DataNascUsuario = Convert.ToDateTime(dr["DataNascUsuario"]);
+                    obj.TipoUsuario_Id = dr["TipoUsuario_Id"].ToString();
+                }
+                return obj;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lascou" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+
+
+
+        }
+
+        //insert
+        public void Cadastrar(UsuarioDTO objCad)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("INSERT INTO usuario (Nome,Email,Senha,DataNascUsuario,TipoUsuario_Id) VALUES(@Nome,@Email,@Senha,@DataNascUsuario,@TipoUsuario_Id)", conn);
+                cmd.Parameters.AddWithValue("@Nome", objCad.Nome);
+                cmd.Parameters.AddWithValue("Email", objCad.Email);
+                cmd.Parameters.AddWithValue("@Senha", objCad.Senha);
+                cmd.Parameters.AddWithValue("@DataNascUsuario", objCad.DataNascUsuario);
+                cmd.Parameters.AddWithValue("@TipoUsuario_Id", objCad.TipoUsuario_Id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+
+
+                throw new Exception("Deu ruimkkk" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //update
+        public void Update(UsuarioDTO objUpdt)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("UPDATE Usuario SET Nome = @Nome Email = Email, Senha= @Senha, DataNascUsuario = @DataNascUsuario, DataNascUsuario = @DataNascUsuario  WHERE Usuario.ID = @ID;", conn);
+                cmd.Parameters.AddWithValue("@Nome", objUpdt.Nome);
+                cmd.Parameters.AddWithValue("Email", objUpdt.Email);
+                cmd.Parameters.AddWithValue("@Senha", objUpdt.Senha);
+                cmd.Parameters.AddWithValue("@DataNascUsuario", objUpdt.DataNascUsuario);
+                cmd.Parameters.AddWithValue("@TipoUsuario_Id", objUpdt.TipoUsuario_Id);
+                cmd.Parameters.AddWithValue("@ID", objUpdt.ID);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+
+
+                throw new Exception("Deu ruimkkk" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //delete
+        public void Delete(int objDel)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("DELETE FROM usuario WHERE Usuario.ID = @ID;", conn);
+                cmd.Parameters.AddWithValue("@ID", objDel);
+                cmd.ExecuteNonQuery ();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lascou" + ex.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+    }
+}
