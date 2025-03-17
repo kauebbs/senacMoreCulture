e17af56484bee97eca48d416f80b036a7b3e9b98
Kauê BBS <120749036+kauebbs@users.noreply.github.com>
2025-03-17 16:11:08 -0300

    Add files via upload

---
+++ EnxamePhobos.BLL/FilmeBLL.cs
@@ -0,0 +1,64 @@
+﻿using EnxamePhobos.DAL;//
+using EnxamePhobos.DTO;//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnxamePhobos.BLL
+{
+    public class FilmeBLL
+    {
+        //objeto para acessar todos os metodos da DAL
+        FilmeDAL objBLL = new FilmeDAL();
+
+        //listar
+
+        public List<FilmeDTO> ListarFilme()
+        {
+            return objBLL.Listar();
+        }
+
+        //busca por nome
+        public FilmeDTO SearchByFilme(string objSearch)
+        {
+            return objBLL.SearchFilme(objSearch);
+        }
+       
+        //busca por ID
+        public FilmeDTO SearchById(int objSearch)
+        {
+            return objBLL.SearchId(objSearch);
+        }
+
+
+        public List<GeneroDTO> CarregarDDList()
+        {
+            return objBLL.CarregaDDL();
+        }
+
+        public void CadastraFilmeBLL(FilmeDTO objCad)
+        {
+            objBLL.CadastrarFilme(objCad);
+        }
+
+        //update
+        public void UpdateFilme(FilmeDTO objUpdt)
+        {
+            objBLL.Update(objUpdt);
+        }
+
+        //delete
+        public void DeleteFilme(int objDel)
+        {
+            objBLL.Delete(objDel);
+        }
+
+        //Filter
+        public List<FilmeDTO> FiltrarFilmeBLL(string objFilter)
+        {
+            return objBLL.FiltrarFilme(objFilter);
+        }
+    }
+}
---
+++ EnxamePhobos.BLL/UsuarioBLL.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnxamePhobos.DAL;
+using EnxamePhobos.DTO;
+
+namespace EnxamePhobos.BLL
+{
+    public class UsuarioBLL
+    {
+        //objeto para acessar todos os metodos da DAL
+        UsuarioDAL objBLL = new UsuarioDAL();
+
+        //autenticar
+        public UsuarioDTO AutenticarUsuario(string objNome, string objSenha)
+
+        {
+            UsuarioDTO user = objBLL.Autenticar(objNome, objSenha);
+            if (user != null)
+            {
+                return user;
+            }
+            else 
+            {
+                throw new Exception("Usuário não cadastrado !");
+            }
+            
+        }
+
+        //listar
+
+        public List<UsuarioDTO> ListarUsuario()
+        {
+            return objBLL.Listar();
+        }
+
+        //carregar dropdownlist
+        public List<TipoUsuarioDTO> CarregaDDList()
+        {
+            return objBLL.CarregaDDL();
+        }
+
+        //busca por nome
+        public UsuarioDTO SearchByName(string objsearch)
+        {
+            return objBLL.SearchName(objsearch);
+        }
+
+        //busca por ID
+        public UsuarioDTO SearchByID(int objsearch)
+        {
+            return objBLL.SearchID(objsearch);
+        }
+
+        //insert
+        public void CadastrarUsuario(UsuarioDTO objCad)
+        {
+            objBLL.Cadastrar(objCad);
+        }
+
+        //update
+        public void UpdateUser(UsuarioDTO objUpdt)
+        {
+            objBLL.Update(objUpdt);
+        }
+
+        //delete
+        public void DeleteUser(int objDel)
+        {
+            objBLL.Delete(objDel);
+        }
+    }
+
+
+}
